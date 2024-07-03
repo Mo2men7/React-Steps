0d56dcae7f130eb1b02d634862ac3962046b9f70
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-03 03:27:06 +0300

    Add another state showing the power of state

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
   const purpleWhite = { background: "#7950f2", color: "#fff" };
 
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -18,26 +19,33 @@ export default function App() {
     if (step < 3) setStep(step + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <span>&times;</span> : "v"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        {/* if we write onClick={alert("lol")} or handlePrevious() it's a call so when the app starts it will execute it even if we didn't click the button, And if we did it will not call the function as it called it at the start of it */}
-        <button style={purpleWhite} onClick={handlePrevious}>
-          Previous
-        </button>
-        <button style={purpleWhite} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+          <div className="buttons">
+            {/* if we write onClick={alert("lol")} or handlePrevious() it's a call so when the app starts it will execute it even if we didn't click the button, And if we did it will not call the function as it called it at the start of it */}
+            <button style={purpleWhite} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={purpleWhite} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
